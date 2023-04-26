da67a9ebfee350cb8b50cdfbd5e0f2109a07b79b
tamaya0018 <shizuoka461139@gmail.com>
2023-04-27 01:38:56 +0900

    add pull down menu
    デバイスからビデオデバイスを取得して、プルダウンメニューから選択できるようにした。
    プルダウンに表示されるだけでどっかの変数に格納してるわけではないのでまだ使えるわけではないけど、そんなかんじ。

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 // List cameras and microphones.
-navigator.mediaDevices.enumerateDevices()
-  .then(devices => {
-    devices.forEach(device => {
-      console.log(device.kind + ": " + device.label + " id = " + device.deviceId);
-    });
-  })
-  .catch(err => {
-    console.log(err.name + ": " + err.message);
-  })
+function addVideoDeviceOption() {
+  var select = document.getElementById("videoDeviceList");
+
+  // get all video device option to pulldown.
+  navigator.mediaDevices.enumerateDevices()
+    .then(devices => {
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+      videoDevices.forEach(device => {
+        console.log(device.kind + ": " + device.label + " id = " + device.deviceId);
+        var option = document.createElement("option");
+        option.text = device.label;
+        select.appendChild(option);
+      });
+    })
+    .catch(err => {
+      console.log(err.name + ": " + err.message);
+    })
+}
